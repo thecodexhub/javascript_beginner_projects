7d7b6d6c9084b4f9c235060357b47c194277ab6f
Sandip <sandipramanik07@gmail.com>
2021-02-21 12:26:50 +0530

    Added Checked Mark and Trash functionality

--- Todo project/app.js
+++ Todo project/app.js
@@ -5,18 +5,23 @@ const todoList = document.querySelector(".todo-list");
 
 // event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // functions
 function addTodo(event) {
-    // Prevent form re-submitting
-    event.preventDefault();
+  // Prevent form re-submitting
+  event.preventDefault();
+
+  if (todoInput.value.trim() === "" || todoInput.value == null) {
+    todoInput.value = "";
+  } else {
     // Create Todo DIV
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     // Create Todo LI items
     const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
+    newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     // Completed Button
     const completedButton = document.createElement("button");
@@ -32,4 +37,22 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     // Clear input value
     todoInput.value = "";
+  }
+}
+
+function deleteCheck(event) {
+  const item = event.target;
+  // Delete Todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  // Check Todo
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
