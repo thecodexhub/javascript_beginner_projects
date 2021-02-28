65d7239366093f3ec6cb8fd54c4f0111dc9b6f17
Sandip <sandipramanik07@gmail.com>
2021-03-01 01:19:30 +0530

    Added Countdown functionality - with end date 1 Jan 2022

---
+++ Countdown project/app.js
@@ -0,0 +1,34 @@
+const newYear = "1 Jan 2022";
+
+const daysCountdown = document.getElementById("days");
+const hoursCountdown = document.getElementById("hours");
+const minutesCountdown = document.getElementById("minutes");
+const secondsCountdown = document.getElementById("seconds");
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const timeDifference = newYearDate - currentDate;
+
+  const totalSeconds = timeDifference / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+  //   const minutes = Math.floor((totalSeconds / 60) - ((days * 24) + hours) * 60);
+  const minutes = Math.floor((totalSeconds / 60) % 60);
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysCountdown.innerText = days;
+  hoursCountdown.innerText = formateTime(hours);
+  minutesCountdown.innerText = formateTime(minutes);
+  secondsCountdown.innerText = formateTime(seconds);
+}
+
+function formateTime (time) {
+    return time < 10 ? `0${time}` : `${time}`; 
+}
+
+countdown();
+
+setInterval(countdown, 1000);
